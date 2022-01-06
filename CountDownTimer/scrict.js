ebde3cbebf62e2c6c9d158b3fa1bfe48ff26ee89
Shubham Singh <87699417+detonators@users.noreply.github.com>
2022-01-07 01:06:27 +0530

    Update scrict.js

--- CountDownTimer/scrict.js
+++ CountDownTimer/scrict.js
@@ -3,13 +3,13 @@ const hoursEl = document.getElementById("hours");
 const minsEl = document.getElementById("mins");
 const secondsEl = document.getElementById("seconds");
 
-const newYears = "1 Jan 2021";
+const newYears = "1 Jan 2022";     // date to be counted
 
 function countdown() {
     const newYearsDate = new Date(newYears);
     const currentDate = new Date();
 
-    const totalSeconds = (newYearsDate - currentDate) / 1000;
+    const totalSeconds = (newYearsDate - currentDate) / 1000;                           // days count
 
     const days = Math.floor(totalSeconds / 3600 / 24);
     const hours = Math.floor(totalSeconds / 3600) % 24;
@@ -20,7 +20,7 @@ function countdown() {
     hoursEl.innerHTML = formatTime(hours);
     minsEl.innerHTML = formatTime(mins);
     secondsEl.innerHTML = formatTime(seconds);
-}
+}                                                           // Days, hours, min, sec will be counted
 
 function formatTime(time) {
     return time < 10 ? `0${time}` : time;
